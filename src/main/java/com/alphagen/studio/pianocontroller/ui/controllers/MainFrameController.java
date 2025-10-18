b78b3eee2c1de6e82acc122fe780fa949e215251
Aadarsh Devi <aadarsh123456char@gmail.com>
2025-10-18 14:33:34 -0700

    Connecting to Piano - created new buttons: reset and settings - reset: resets data - settings: does nothing
    Before I can run it will check if SaveReader, Config File and MidiDevice are usable, not null, empty, not connectable, etc.

--- src/main/java/com/alphagen/studio/pianocontroller/ui/controllers/MainFrameController.java
+++ src/main/java/com/alphagen/studio/pianocontroller/ui/controllers/MainFrameController.java
@@ -1,18 +1,23 @@
 package com.alphagen.studio.pianocontroller.ui.controllers;
 
 import com.alphagen.studio.pianocontroller.Main;
+import com.alphagen.studio.pianocontroller.midi.MidiDeviceChecker;
+import com.alphagen.studio.pianocontroller.midi.MidiDeviceReceiver;
 import com.alphagen.studio.pianocontroller.save.SaveReader;
 import com.alphagen.studio.pianocontroller.save.SaveWriter;
 import com.alphagen.studio.pianocontroller.ui.managers.ControllerManager;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import jfilesystem.file.JFSFile;
@@ -24,14 +29,16 @@ import javax.sound.midi.MidiDevice;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
+import java.util.HashMap;
 
 public class MainFrameController {
     private static final Logger logger = LogManager.getLogger(MainFrameController.class);
 
     boolean selectMidiUIOpen;
+    boolean isRunning = false;
     private File configFile;
     private MidiDevice midiDevice;
-
+    private SaveReader saveReader;
     @FXML
     private Label configFileNameLabel;
     @FXML
@@ -40,28 +47,29 @@ public class MainFrameController {
     private Button config_file_create;
     @FXML
     private Button config_file_save;
-
     @FXML
     private Label midiDeviceNameLabel;
     @FXML
     private Button selectMidiDeviceButton;
-
     @FXML
     private Label ui_type_name;
     @FXML
     private Button ui_type_select;
-
     @FXML
-    private Button app_run;
+    private Button runAppButton;
+    @FXML
+    private HBox configButtonContainer;
 
     @FXML
     public void initialize() {
         logger.info("Initialized");
         selectMidiUIOpen = false;
+        runAppButton.setTextFill(Paint.valueOf("#ffffff"));
+        isRunning();
 
         // test
-        configFile = new JFSFile("D:/piano_config_1.piano.txt");
-        logger.info("Set Config file: " + configFile.getAbsolutePath());
+//        configFile = new JFSFile("D:/piano_config_1.piano.txt");
+//        logger.info("Set Config file: " + configFile.getAbsolutePath());
         // test
     }
 
@@ -74,27 +82,30 @@ public class MainFrameController {
             fileChooser.setTitle("Choose Piano Config File");
             fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Piano Config", "*.piano.txt"));
             config = fileChooser.showOpenDialog(null);
+            validConfigFile();
         }
-        config = configFile; // test
+//        config = configFile; // test
         if (config != null) {
+            validConfigFile();
             this.configFile = config;
             logger.info("Loaded Config File: {}", config.getName());
             configFileNameLabel.setText(this.configFile.getName());
             try {
-                SaveReader saveReader = new SaveReader(new JFSFile(configFile.getAbsolutePath()));
+                saveReader = new SaveReader(new JFSFile(configFile.getAbsolutePath()));
                 saveReader.readLetters();
                 saveReader.readNum();
                 saveReader.readNumPad();
                 saveReader.readSpecials();
                 saveReader.readMouseClicks();
                 saveReader.currentSave();
-                System.exit(0);// test
+//                System.exit(0);// test
             } catch (IOException e) {
                 throw new RuntimeException(e);
             } catch (JSFFileNullException e) {
                 logger.error("Unable to get File");
             }
         }
+        validConfigFile();
     }
 
     @FXML
@@ -111,26 +122,25 @@ public class MainFrameController {
         fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
 
         // Set extension filters, e.g., TXT files only
-        fileChooser.getExtensionFilters().add(
-                new FileChooser.ExtensionFilter("Piano Config", "*.piano.txt")
-        );
-        Stage stage = new Stage();
-        File file = fileChooser.showSaveDialog(stage);
-        JFSFile jfsFile = new JFSFile(file.getAbsolutePath());
-        try {
-            boolean success = jfsFile.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        if (!jfsFile.exists()) {
-            logger.error("Unable to create Piano Config");
-            return;
-        }
-        try {
-            SaveWriter saveWriter = new SaveWriter(jfsFile);
-            saveWriter.newSave();
-        } catch (FileAlreadyExistsException e) {
-            throw new RuntimeException(e);
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Piano Config", "*.piano.txt"));
+        File file = fileChooser.showSaveDialog(new Stage());
+        if (file != null) {
+            JFSFile jfsFile = new JFSFile(file.getAbsolutePath());
+            try {
+                boolean success = jfsFile.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            if (!jfsFile.exists()) {
+                logger.error("Unable to create Piano Config");
+                return;
+            }
+            try {
+                SaveWriter saveWriter = new SaveWriter(jfsFile);
+                saveWriter.newSave();
+            } catch (FileAlreadyExistsException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -189,7 +199,51 @@ public class MainFrameController {
 
     @FXML
     public void runApplication() {
-        System.out.println("run");
+        logger.info("run");
+
+        validConfigFile();
+
+        if (saveReader == null) {
+            logger.error("Piano Config Reader is Null");
+            validConfigFile();
+            return;
+        }
+
+        HashMap<Integer, Integer> keyMap = saveReader.getKeyMap();
+        HashMap<Integer, Integer> mouseMap = saveReader.getMouseMap();
+
+        if (keyMap == null || mouseMap == null || keyMap.isEmpty()) {
+            logger.error("Data is unparsed or empty");
+            configButtonContainer.setBorder(new Border(new BorderStroke(
+                    Color.RED,
+                    BorderStrokeStyle.SOLID,
+                    CornerRadii.EMPTY,
+                    BorderWidths.DEFAULT
+            )));
+            return;
+        } else {
+            configButtonContainer.setBorder(null);
+        }
+
+        if (midiDevice == null) {
+            logger.error("There is no Midi Device connected");
+            midiDeviceNameLabel.setTextFill(Color.WHITE);
+            midiDeviceNameLabel.setBackground(new Background(new BackgroundFill(Paint.valueOf("#9c0000"), new CornerRadii(0), Insets.EMPTY)));
+            return;
+        } else {
+            midiDeviceNameLabel.setTextFill(Color.BLACK); // default text color
+            midiDeviceNameLabel.setBackground(new Background(new BackgroundFill(Paint.valueOf("#f4f4f4"), new CornerRadii(0), Insets.EMPTY)));
+        }
+
+        if (!MidiDeviceChecker.checkDevice(midiDevice)) {
+            logger.error("Unable to connect to device");
+            return;
+        }
+
+        isRunning();
+
+        MidiDeviceReceiver mdr = new MidiDeviceReceiver(midiDevice.getDeviceInfo().getName());
+        Thread thread = new Thread(mdr);
     }
 
     public void setMidiDevice(MidiDevice midiDevice) {
@@ -200,5 +254,53 @@ public class MainFrameController {
 
     public File getConfigFile() {
         return configFile;
+    }
+
+    public void isRunning() {
+        if (isRunning) {
+            runAppButton.setText("⏹");
+            runAppButton.setPrefWidth(30);
+            runAppButton.setBackground(new Background(new BackgroundFill(Paint.valueOf("#bf3434"), new CornerRadii(5), Insets.EMPTY)));
+            isRunning = false;
+        } else {
+            runAppButton.setText("▶");
+            runAppButton.setPrefWidth(30);
+            runAppButton.setBackground(new Background(new BackgroundFill(Paint.valueOf("#42bf34"), new CornerRadii(5), Insets.EMPTY)));
+            isRunning = true;
+        }
+    }
+
+    @FXML
+    public void openSettings() {
+    }
+
+    @FXML
+    public void resetData() {
+        midiDevice = null;
+        saveReader = null;
+        configFile = null;
+        isRunning = false;
+
+        configFileNameLabel.setTextFill(Color.BLACK); // default text color
+        configFileNameLabel.setText("Config File");
+        configFileNameLabel.setBackground(new Background(new BackgroundFill(Paint.valueOf("#f4f4f4"), new CornerRadii(0), Insets.EMPTY)));
+
+        midiDeviceNameLabel.setTextFill(Color.BLACK); // default text color
+        midiDeviceNameLabel.setText("Midi Device");
+        midiDeviceNameLabel.setBackground(new Background(new BackgroundFill(Paint.valueOf("#f4f4f4"), new CornerRadii(0), Insets.EMPTY)));
+
+        isRunning();
+    }
+
+    public void validConfigFile() {
+        if (saveReader == null) {
+            logger.error("ConfigReader is null");
+            configFileNameLabel.setTextFill(Color.WHITE);
+            configFileNameLabel.setBackground(new Background(new BackgroundFill(Paint.valueOf("#9c0000"), new CornerRadii(0), Insets.EMPTY)));
+            return;
+        } else {
+            configFileNameLabel.setTextFill(Color.BLACK); // default text color
+            configFileNameLabel.setBackground(new Background(new BackgroundFill(Paint.valueOf("#f4f4f4"), new CornerRadii(0), Insets.EMPTY)));
+        }
     }
 }
