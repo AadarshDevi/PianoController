20536107e862162048dd535d41515f3ea7452838
Aadarsh Devi <aadarsh123456char@gmail.com>
2025-10-19 16:41:36 -0700

    trying to get piano keys to map to keyboard keys and press the corresponding letter/key

--- src/main/java/com/alphagen/studio/pianocontroller/midi/MidiDeviceReceiver.java
+++ src/main/java/com/alphagen/studio/pianocontroller/midi/MidiDeviceReceiver.java
@@ -6,31 +6,50 @@ import org.apache.logging.log4j.Logger;
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.Receiver;
 import javax.sound.midi.ShortMessage;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
 
 public class MidiDeviceReceiver implements Receiver, Runnable {
     private static final Logger logger = LogManager.getLogger(MidiDeviceReceiver.class);
+    private Robot robot;
+    private HashMap<Integer, Integer> keyMap;
+    private HashMap<Integer, Integer> mouseMap;
 
     public MidiDeviceReceiver(String name) {
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            logger.error(e);
+        }
+        keyMap = new HashMap<>();
+        mouseMap = new HashMap<>();
     }
 
-    @Override
-    public void run() {
-
+    @Override public void run() {
     }
 
-    @Override
-    public void send(MidiMessage message, long timeStamp) {
+    @Override public void send(MidiMessage message, long timeStamp) {
         ShortMessage sm = (ShortMessage) message;
         int cmd = sm.getCommand();
         int key = sm.getData1();
         int velocity = sm.getData2();
-        if (velocity > 0) {
-            System.out.println(key + ", " + cmd + ", " + velocity);
+        if (cmd == ShortMessage.NOTE_ON && velocity > 0) {
+            System.out.println("pressed: " + key);
+            if (keyMap.containsKey(key)) {
+                System.out.println("key: " + key + ", value: " + keyMap.get(key));
+                robot.keyPress(keyMap.get(key));
+            } else {
+                System.out.println("value of key not found: " + key);
+            }
+        } else if (cmd == ShortMessage.NOTE_OFF) {
+            robot.delay(50);
+            System.out.println("released: " + key);
+            if (keyMap.containsKey(key)) robot.keyRelease(keyMap.get(key));
         }
     }
 
-    @Override
-    public void close() {
+    @Override public void close() {
 
     }
 }
