81b81d16494380e1390ddfb5d1924591b48372fe
Aadarsh Devi <aadarsh123456char@gmail.com>
2025-10-22 21:47:27 -0700

    Added new UI Controllers and Objects

--- src/main/java/com/alphagen/studio/pianocontroller/ui/managers/ControllerManager.java
+++ src/main/java/com/alphagen/studio/pianocontroller/ui/managers/ControllerManager.java
@@ -1,32 +1,50 @@
 package com.alphagen.studio.pianocontroller.ui.managers;
 
+import com.alphagen.studio.pianocontroller.midi.MidiDeviceReceiver;
+import com.alphagen.studio.pianocontroller.ui.controllers.KeyVisualizerController;
 import com.alphagen.studio.pianocontroller.ui.controllers.MainFrameController;
 import com.alphagen.studio.pianocontroller.ui.controllers.MidiDeviceSelectionController;
 
 public class ControllerManager {
 
-    private static MainFrameController mainFrameController;
-    private static ControllerManager controllerManager;
-    private static MidiDeviceSelectionController midiDeviceSelectionController;
+    private static MainFrameController mainFrameController = null;
+    private static ControllerManager controllerManager = null;
+    private static MidiDeviceSelectionController midiDeviceSelectionController = null;
+    private static KeyVisualizerController keyVisualizerController = null;
+    private static MidiDeviceReceiver midiDeviceReceiver;
 
     public static MainFrameController getMainFrameController() {
         return mainFrameController;
     }
 
     public static void setMainFrameController(MainFrameController mainFrameController) {
-        ControllerManager.mainFrameController = mainFrameController;
-    }
-
-    public static void setMidiDeviceSelectionController(MidiDeviceSelectionController midiDeviceSelectionController) {
-        ControllerManager.midiDeviceSelectionController = midiDeviceSelectionController;
+        if (mainFrameController != null)
+            ControllerManager.mainFrameController = mainFrameController;
     }
 
     public static MidiDeviceSelectionController getMidiDeviceSelectionController() {
         return midiDeviceSelectionController;
     }
 
-    //    public static ControllerManager getInstance() {
-//        if (controllerManager == null) controllerManager = new ControllerManager();
-//        return controllerManager;
-//    }
+    public static void setMidiDeviceSelectionController(MidiDeviceSelectionController midiDeviceSelectionController) {
+        if (mainFrameController != null)
+            ControllerManager.midiDeviceSelectionController = midiDeviceSelectionController;
+    }
+
+    public static KeyVisualizerController getKeyVisualizerController() {
+        return keyVisualizerController;
+    }
+
+    public static void setKeyVisualizerController(KeyVisualizerController keyVisualizerController) {
+        if (mainFrameController != null)
+            ControllerManager.keyVisualizerController = keyVisualizerController;
+    }
+
+    public static void setMidiDeviceReceiver(MidiDeviceReceiver midiDeviceReceiver) {
+        ControllerManager.midiDeviceReceiver = midiDeviceReceiver;
+    }
+
+    public static MidiDeviceReceiver getMidiDeviceReceiver() {
+        return midiDeviceReceiver;
+    }
 }
