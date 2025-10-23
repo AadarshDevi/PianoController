a6f32caeee63b838f098cd5f079ed482c0d47417
Aadarsh Devi <aadarsh123456char@gmail.com>
2025-10-22 21:49:59 -0700

    updated by adding export and require

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,12 +6,13 @@ module com.alphagen.studio.pianocontroller {
     requires org.apache.logging.log4j.core;
     requires org.apache.logging.log4j;
     requires JFileSystem;
+    requires javafx.base;
 
 
     exports com.alphagen.studio.pianocontroller;
     exports com.alphagen.studio.pianocontroller.ui.managers to javafx.fxml;
     exports com.alphagen.studio.pianocontroller.ui.controllers to javafx.fxml;
-    exports com.alphagen.studio.pianocontroller.util;
+    exports com.alphagen.studio.pianocontroller.util to javafx.fxml;
 
 
     opens com.alphagen.studio.pianocontroller to javafx.fxml;
