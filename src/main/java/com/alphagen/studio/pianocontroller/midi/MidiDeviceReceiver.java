7afeac388f7c31535e91261fda579f0427f257b0
Aadarsh Devi <aadarsh123456char@gmail.com>
2025-10-22 21:49:35 -0700

    fixed input. did not get the key map from save reader

--- src/main/java/com/alphagen/studio/pianocontroller/midi/MidiDeviceReceiver.java
+++ src/main/java/com/alphagen/studio/pianocontroller/midi/MidiDeviceReceiver.java
@@ -1,29 +1,44 @@
 package com.alphagen.studio.pianocontroller.midi;
 
+import com.alphagen.studio.pianocontroller.ui.controllers.KeyVisualizerController;
+import com.alphagen.studio.pianocontroller.ui.managers.ControllerManager;
+import com.alphagen.studio.pianocontroller.ui.swing.KeyVisualizer;
+import com.alphagen.studio.pianocontroller.util.WindowUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.sound.midi.MidiMessage;
 import javax.sound.midi.Receiver;
 import javax.sound.midi.ShortMessage;
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.util.HashMap;
 
 public class MidiDeviceReceiver implements Receiver, Runnable {
     private static final Logger logger = LogManager.getLogger(MidiDeviceReceiver.class);
+    //    public final LinkedBlockingQueue<Integer> blockingQueue;
+    private final String[] keys = new String[]{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+    private final HashMap<Integer, Integer> keyMap;
+    //    private final HashMap<Integer, Integer> mouseMap;
+    private final KeyVisualizerController keyVisualizerController;
+    //    public JFrame jFrame;
+    private KeyVisualizer visualizer;
     private Robot robot;
-    private HashMap<Integer, Integer> keyMap;
-    private HashMap<Integer, Integer> mouseMap;
 
     public MidiDeviceReceiver(String name) {
         try {
             robot = new Robot();
         } catch (AWTException e) {
             logger.error(e);
+            WindowUtil.quit();
         }
-        keyMap = new HashMap<>();
-        mouseMap = new HashMap<>();
+
+        visualizer = new KeyVisualizer();
+
+        keyMap = ControllerManager.getMainFrameController().getSaveReader().getKeyMap();
+//        mouseMap = new HashMap<>();
+//        blockingQueue = new LinkedBlockingQueue<>();
+        keyVisualizerController = ControllerManager.getKeyVisualizerController();
     }
 
     @Override public void run() {
@@ -34,22 +49,39 @@ public class MidiDeviceReceiver implements Receiver, Runnable {
         int cmd = sm.getCommand();
         int key = sm.getData1();
         int velocity = sm.getData2();
-        if (cmd == ShortMessage.NOTE_ON && velocity > 0) {
-            System.out.println("pressed: " + key);
-            if (keyMap.containsKey(key)) {
-                System.out.println("key: " + key + ", value: " + keyMap.get(key));
-                robot.keyPress(keyMap.get(key));
-            } else {
-                System.out.println("value of key not found: " + key);
-            }
-        } else if (cmd == ShortMessage.NOTE_OFF) {
-            robot.delay(50);
-            System.out.println("released: " + key);
-            if (keyMap.containsKey(key)) robot.keyRelease(keyMap.get(key));
+        if (cmd == ShortMessage.NOTE_ON && velocity > 0 && keyMap.containsKey(key)) {
+            int value = keyMap.get(key);
+            robot.keyPress(value);
+            logger.info("key: {}, value: {}", key, value);
+
+            SwingUtilities.invokeLater(() -> {
+                visualizer.getPianoKeyNumLabel().setText("PK#: " + key);
+                visualizer.getPianoKeyNameLabel().setText("KN: " + keys[key % 12]);
+                visualizer.getKeyboardKeyNumLabel().setText("KK#: " + value);
+            });
+
+//            try {
+//                blockingQueue.put(key);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+            keyVisualizerController.setKey(key, keyMap.get(key));
+            logger.info("keyPressed");
+        } else if (keyMap.containsKey(key)) {
+            logger.info("released key: {}", key);
+            robot.keyRelease(keyMap.get(key));
         }
     }
 
+//    public LinkedBlockingQueue<Integer> getPianoKeyList() {
+//        return blockingQueue;
+//    }
+
     @Override public void close() {
 
+    }
+
+    public JFrame getKeyViewer() {
+        return visualizer.getRoot();
     }
 }
