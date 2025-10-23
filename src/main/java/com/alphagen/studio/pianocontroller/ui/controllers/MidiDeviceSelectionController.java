e0205279b23feeb165cf839455a6407e6d925b4b
Aadarsh Devi <aadarsh123456char@gmail.com>
2025-10-22 21:49:11 -0700

    improved midi device selection: ui and module

--- src/main/java/com/alphagen/studio/pianocontroller/ui/controllers/MidiDeviceSelectionController.java
+++ src/main/java/com/alphagen/studio/pianocontroller/ui/controllers/MidiDeviceSelectionController.java
@@ -1,31 +1,25 @@
 package com.alphagen.studio.pianocontroller.ui.controllers;
 
-import com.alphagen.studio.pianocontroller.Main;
-import com.alphagen.studio.pianocontroller.midi.MidiDeviceChecker;
 import com.alphagen.studio.pianocontroller.ui.controllers.modules.MidiDeviceModuleController;
 import com.alphagen.studio.pianocontroller.ui.managers.ControllerManager;
-import javafx.collections.FXCollections;
+import com.alphagen.studio.pianocontroller.util.WindowUtil;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiMessage;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import java.io.IOException;
 
 public class MidiDeviceSelectionController {
     private static final Logger logger = LogManager.getLogger(MidiDeviceSelectionController.class);
     @FXML
+//    private HBox root;
     private VBox rootPane;
     @FXML
     private ListView<BorderPane> midiDeviceListView;
@@ -35,40 +29,8 @@ public class MidiDeviceSelectionController {
     private Button useSelectedMidiDeviceButton;
     private MidiDevice midiDevice;
     private boolean testingConnection = false;
+//    @FXML private Button quitAppButton;
 
-    public void populateList() {
-        logger.info("Selecting Midi Device");
-        ObservableList<MidiDevice.Info> observableList = FXCollections.observableArrayList(MidiSystem.getMidiDeviceInfo());
-
-        for (MidiDevice.Info mdi : observableList) {
-            try {
-                if (!MidiDeviceChecker.checkDevice(MidiSystem.getMidiDevice(mdi))) continue;
-            } catch (MidiUnavailableException e) {
-                throw new RuntimeException(e);
-            }
-
-            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/MidiDeviceModule_V1.fxml"));
-            BorderPane module = null;
-            try {
-                module = fxmlLoader.load();
-                module.setPrefWidth(midiDeviceListView.getPrefWidth() - 20);
-            } catch (IOException e) {
-                logger.error("Unable to load Midi Module FXML");
-            }
-            MidiDeviceModuleController moduleController = fxmlLoader.getController();
-            if (module != null) {
-                module.getProperties().put("controller", moduleController);
-            }
-            try {
-                moduleController.setDevice(mdi);
-            } catch (MidiUnavailableException e) {
-                logger.error("MidiDevice is Unavailable");
-            }
-            midiDeviceListView.getItems().add(module);
-        }
-
-        logger.info("Added all MidiDevice");
-    }
 
     public void setMidiDevice(MidiDevice midiDevice) {
         this.midiDevice = midiDevice;
@@ -78,16 +40,25 @@ public class MidiDeviceSelectionController {
     }
 
     public void unique() {
-        ObservableList<BorderPane> observableList = midiDeviceListView.getItems();
-        for (BorderPane module : observableList) {
-            MidiDeviceModuleController mdc = (MidiDeviceModuleController) module.getProperties().get("controller");
-            MidiDevice checkDevice = mdc.getMidiDevice();
-            if (!checkDevice.equals(midiDevice)) {
-                mdc.reset();
-            } else {
-                currentMidiDeviceNameLabel.setText(checkDevice.getDeviceInfo().getName());
-            }
-        }
+//        ObservableList<BorderPane> observableList = midiDeviceListView.getItems();
+//        for (BorderPane module : observableList) {
+//            MidiDeviceModuleController mdc = (MidiDeviceModuleController) module.getProperties().get("controller");
+//            MidiDevice checkDevice = mdc.getMidiDevice();
+//            if (!checkDevice.equals(midiDevice)) {
+//                mdc.reset();
+//            } else {
+//                currentMidiDeviceNameLabel.setText(checkDevice.getDeviceInfo().getName());
+//            }
+//        }
+        midiDeviceListView
+                .getItems()
+                .stream()
+                .map(module -> (MidiDeviceModuleController) module.getProperties().get("controller"))
+                .forEach(mmc -> {
+                    MidiDevice md = mmc.getMidiDevice();
+                    if (!md.equals(midiDevice)) mmc.reset();
+                    else currentMidiDeviceNameLabel.setText(md.getDeviceInfo().getName());
+                });
     }
 
     @FXML
@@ -95,37 +66,33 @@ public class MidiDeviceSelectionController {
         if (midiDevice != null) {
             ControllerManager.getMainFrameController().setMidiDevice(midiDevice);
             testingConnection = false;
-            close();
+//            logger.info(root.getWidth());
+            WindowUtil.close(rootPane);
         }
     }
 
+    @FXML
     public void close() {
-        Stage stage = (Stage) rootPane.getScene().getWindow();
-        ControllerManager.getMainFrameController().setSelectMidiUIOpen(false);
-        logger.info("Closing Midi Selection Window");
-        stage.close();
+        WindowUtil.close(rootPane);
     }
-
-    public void populateList(MidiDevice midiDevice) {
-        populateList();
-        ObservableList<BorderPane> observableList = midiDeviceListView.getItems();
-        for (BorderPane module : observableList) {
-            MidiDeviceModuleController mdc = (MidiDeviceModuleController) module.getProperties().get("controller");
-            MidiDevice checkDevice = mdc.getMidiDevice();
-            if (!checkDevice.equals(midiDevice)) {
-                mdc.reset();
-            } else {
-                mdc.active();
-                this.midiDevice = midiDevice;
-                logger.info("Midi Module Activated");
-            }
-        }
-        logger.info("Set MidiDevice Selection");
-    }
-
 
     public boolean isTestingConnection() {
         return testingConnection;
+    }
+
+    public ListView<BorderPane> getListView() {
+        return midiDeviceListView;
+    }
+
+    public MidiDevice setMidiDevice() {
+        return midiDevice;
+    }
+
+    //    public HBox getRoot() {
+//        return root;
+//    }
+    public VBox getRoot() {
+        return rootPane;
     }
 
     public void setTestCode(MidiMessage message) {
